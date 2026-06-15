120fe39f2da7ca03f28961324ac08dd61feeebac
modulomedito <chcchc1995@outlook.com>
2026-06-15 10:48:47 +0800

    fix(keymaps): replace gg/gf with leader-prefixed keys
    update all plugin key mappings to use leader prefix to avoid conflicts with default neovim built-in keys

--- lua/rookie_rg/keymaps.lua
+++ lua/rookie_rg/keymaps.lua
@@ -4,15 +4,15 @@ function M.setup()
   local core = require("rookie_rg.core")
   local opts = { silent = true }
 
-  vim.keymap.set("n", "gg", core.global_grep, vim.tbl_extend("force", opts, {
+  vim.keymap.set("n", "<leader>gg", core.global_grep, vim.tbl_extend("force", opts, {
     desc = "RookieRg: grep word under cursor",
   }))
 
-  vim.keymap.set("x", "gg", core.visual_grep, vim.tbl_extend("force", opts, {
+  vim.keymap.set("x", "<leader>gg", core.visual_grep, vim.tbl_extend("force", opts, {
     desc = "RookieRg: grep visual selection",
   }))
 
-  vim.keymap.set("n", "gf", core.live_grep, vim.tbl_extend("force", opts, {
+  vim.keymap.set("n", "<leader>gf", core.live_grep, vim.tbl_extend("force", opts, {
     desc = "RookieRg: prompt for grep pattern",
   }))
 
